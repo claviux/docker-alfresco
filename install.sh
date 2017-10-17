5007c48243af9818d511281997e74b35429e36d1
davidgoitia <david@claviux.com>
2017-10-17 18:09:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 ./alfresco-community-installer-201707-linux-x64.bin --mode unattended --alfresco_admin_password admin --disable-components alfrescovalidations
-# rm ./alfresco-community-installer-201707-linux-x64.bin
+rm ./alfresco-community-installer-201707-linux-x64.bin
 # mv /apply_amps_unatended.sh /opt/alfresco-community/bin
 # mv /alfrescotrashcancleaner2.amp /opt/alfresco-community/amps
 # mv /protectnodes.amp /opt/alfresco-community/amps
